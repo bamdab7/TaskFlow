0e59cc99c9d122ed4477cd347e3865ba02d75243
nekoo <nereapena42@gmail.com>
2023-05-05 10:33:50 +0200

    subiendo iconos a la aplicacion

--- TaskFlow/src/main/java/com/taskflow/taskflow/HomeController.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/HomeController.java
@@ -3,7 +3,7 @@ package com.taskflow.taskflow;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomeController {
     @FXML
     private Label welcomeText;
 
