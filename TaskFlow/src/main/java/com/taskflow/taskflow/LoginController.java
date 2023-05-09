248521abfbcf8ef9914796633e96a060dac4436f
nekoo <nereapena42@gmail.com>
2023-05-09 16:13:55 +0200

    terminando proceso de login + comprobacion de usuario y mensaje de que no existe

--- TaskFlow/src/main/java/com/taskflow/taskflow/LoginController.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/LoginController.java
@@ -1,6 +1,7 @@
 package com.taskflow.taskflow;
 
 import com.jfoenix.controls.JFXButton;
+import com.taskflow.taskflow.dao.UsuariosDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,6 +10,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,22 +25,43 @@ public class LoginController implements Initializable {
     public TextField txtPassword;
     public JFXButton btnLogin;
     public Hyperlink linkRegister;
+    public Text verification;
+
+    UsuariosDAO usuariosDAO;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Calling UsuariosDAO in order to get the users loged
+        usuariosDAO = new UsuariosDAO();
+        try {
+            usuariosDAO.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("Conexion conseguida con bd usuarios\n");
 
     }
 
     public void btnLogin(ActionEvent actionEvent) throws IOException, SQLException {
-        //Scene Home
-           // Parent fxmlLoaderHome = FXMLLoader.load(TaskFlowApplication.class.getResource("home-view.fxml"));
-        Scene sceneHome = TaskFlowApplication.sceneHome;
-        Stage stageHome = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        //Check credentials
+        String username = txtUser.getText();
+        String password = txtPassword.getText();
+        if(usuariosDAO.comprobarUsuario(username,password)){
+            System.out.println("Usuario encontrado\n");
+            //Scene Home
+            Scene sceneHome = TaskFlowApplication.sceneHome;
+            Stage stageHome = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
-        stageHome.setScene(sceneHome);
-        stageHome.show();
-
+            stageHome.setScene(sceneHome);
+            stageHome.show();
+            System.out.println(username + password);
+        }else{
+            eliminarCampos();
+            verification.setText("Usuario no encontrado");
+            verification.setStyle("-fx-font-size: 16px;");
+            verification.setFill(Color.RED);
+        }
     }
 
     public void linkRegister(ActionEvent actionEvent) {
@@ -47,5 +71,11 @@ public class LoginController implements Initializable {
 
         stageRegister.setScene(sceneRegister);
         stageRegister.show();
+    }
+
+    //Set all fields null
+    public void eliminarCampos(){
+        txtUser.setText("");
+        txtPassword.setText("");
     }
 }
--- TaskFlow/src/main/java/com/taskflow/taskflow/dao/UsuariosDAO.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/dao/UsuariosDAO.java
@@ -1,4 +1,42 @@
 package com.taskflow.taskflow.dao;
 
+import java.sql.*;
+
 public class UsuariosDAO {
+    Connection conn;
+    //Connect to the database
+     public Connection getConnection() throws SQLException {
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/gestion_tareas", "root", "");
+            //System.out.print("Conexion establecida");
+            return conn;
+        }catch (SQLException e){
+            //Catching errors
+            System.out.printf("Error " + e.getMessage());
+            return  null;
+        }
+    }
+
+    //Methods
+
+    //Find user and password
+    public boolean comprobarUsuario(String username, String password) throws SQLException {
+         try{
+            Connection  conn = getConnection();
+            String query = "SELECT * FROM USUARIOS WHERE username = ? AND password = ?";
+            PreparedStatement st = conn.prepareStatement(query);
+
+            //Saving the values
+             st.setString(1,username);
+             st.setString(2,password);
+
+             ResultSet rs = st.executeQuery();
+             boolean autenticated = rs.next();
+
+             return autenticated;
+         }catch (Exception e){
+             System.out.printf("Error: " +e.getMessage());
+             return false;
+         }
+    }
 }
