404e4ab8fccc0d654a1e9074bbd2be723080a851
nekoo <nereapena42@gmail.com>
2023-05-09 10:51:04 +0200

    lanzando de primero login, preparando scenas para hacer cambio

--- TaskFlow/src/main/java/com/taskflow/taskflow/LoginController.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/LoginController.java
@@ -1,14 +1,31 @@
 package com.taskflow.taskflow;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
+    public TextField txtUser;
+    public TextField txtPassword;
+    public JFXButton btnLogin;
+    public Hyperlink linkRegister;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void btnLogin(ActionEvent actionEvent) {
+        //Scene Home
+    }
+
+    public void linkRegister(ActionEvent actionEvent) {
+        //Scene Registwer
     }
 }
--- TaskFlow/src/main/java/com/taskflow/taskflow/TaskFlowApplication.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/TaskFlowApplication.java
@@ -2,6 +2,7 @@ package com.taskflow.taskflow;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -10,14 +11,27 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class TaskFlowApplication extends Application {
+    //Getting the controller
+    public static LoginController controladorLogin;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TaskFlowApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+       // FXMLLoader fxmlLoaderHome = new FXMLLoader(TaskFlowApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoaderLogin = new FXMLLoader(TaskFlowApplication.class.getResource("login-view.fxml"));
+
+        //Creating scenes
+            // Scene sceneHome = new Scene(fxmlLoaderHome.load(), 600, 400);
+        Scene sceneLogin = new Scene(fxmlLoaderLogin.load(), 600, 400);
+
         //Adding icon to application
         stage.getIcons().add(new Image(Objects.requireNonNull(TaskFlowApplication.class.getResourceAsStream("/logo.png"))));
+
+        //Setting the tittle
         stage.setTitle("Task Flow!");
-        stage.setScene(scene);
+
+        //Launching scene
+            // stage.setScene(sceneHome);
+        stage.setScene(sceneLogin);
+
         //No resize
         stage.setResizable(false);
         stage.show();
