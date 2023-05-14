f310576c49bbbe345297078ddc12e761528e849f
nekoo <nereapena42@gmail.com>
2023-05-14 14:42:02 +0200

    creando los metodos basicos del dialogo, faltaria hacer que al clicar cualquier objeto de la tabla, se abriera el dialogo con la info + hacer los metodos

--- TaskFlow/src/main/java/com/taskflow/taskflow/EditTaskController.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/EditTaskController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class EditTaskController {
     public TextArea txtDescripcion;
@@ -14,15 +15,34 @@ public class EditTaskController {
     public Button btnCancelar;
     public Button btnAceptar;
 
+    private Stage dialogStage;
+    private boolean okClicked = false;
+
     public void btnEliminar(ActionEvent actionEvent) {
         //Deletes the task, and comeback to home view
-    }
+        okClicked = true;
+        dialogStage.close();
 
-    public void btnCancelar(ActionEvent actionEvent) {
-        //Close the window
+        //ACTIONS... DELETE ... UDPATE THE VIEW
+
+
     }
 
     public void btnAceptar(ActionEvent actionEvent) {
         //Update the task and comeback to homeview
+        okClicked = true;
+        dialogStage.close();
+
+        //ACTIONS.... UPDATE ... UPDATE THE VIEW
+
+    }
+
+    public boolean isOkClicked(){
+        return okClicked;
+    }
+
+    public void btnCancelar(ActionEvent actionEvent) {
+        //Close the window
+        dialogStage.close();
     }
 }
