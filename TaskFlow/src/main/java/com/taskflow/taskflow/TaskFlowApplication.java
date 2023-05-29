ad21284a7fa230137798a5575dabaa22dfcea7a9
nekoo <nereapena42@gmail.com>
2023-05-29 19:00:56 +0200

    reestructurando un poco la vista de categorias (en progreso)

--- TaskFlow/src/main/java/com/taskflow/taskflow/TaskFlowApplication.java
+++ TaskFlow/src/main/java/com/taskflow/taskflow/TaskFlowApplication.java
@@ -34,48 +34,49 @@ public class TaskFlowApplication extends Application {
     public static Scene sceneRegister;
     public static Scene sceneLogin;
     public static Scene sceneCategorias;
+
     @Override
     public void start(Stage stage) throws IOException, SQLException {
 
         users.crearDBUsuarios();
-            users.insertarUsuarioDefecto();
+        users.insertarUsuarioDefecto();
         categorias.crearDBCategorias();
-            categorias.insertarCategoriaDefecto();
+        categorias.insertarCategoriaDefecto();
         tareas.crearDBTareas();
-            tareas.insertTareaporDefecto();
+        tareas.insertTareaporDefecto();
 
         //Creating scenes
         FXMLLoader fxmlLoaderHome = new FXMLLoader(TaskFlowApplication.class.getResource("home-view.fxml"));
-            Parent rootHome = fxmlLoaderHome.load();
-            controladorHome = fxmlLoaderHome.getController();
-            sceneHome = new Scene(rootHome,646,394);
+        Parent rootHome = fxmlLoaderHome.load();
+        controladorHome = fxmlLoaderHome.getController();
+        sceneHome = new Scene(rootHome, 646, 394);
 
         FXMLLoader fxmlLoaderRegister = new FXMLLoader(TaskFlowApplication.class.getResource("register-view.fxml"));
-            Parent rootRegister = fxmlLoaderRegister.load();
-            controladorRegister = fxmlLoaderRegister.getController();
-            sceneRegister = new Scene(rootRegister, 656,400);
+        Parent rootRegister = fxmlLoaderRegister.load();
+        controladorRegister = fxmlLoaderRegister.getController();
+        sceneRegister = new Scene(rootRegister, 656, 400);
 
         FXMLLoader fxmlLoaderCategorias = new FXMLLoader(TaskFlowApplication.class.getResource("categories-view.fxml"));
-            Parent rootCategorias = fxmlLoaderCategorias.load();
-            controladorCategorias = fxmlLoaderCategorias.getController();
-            sceneCategorias = new Scene(rootCategorias, 646,394);
+        Parent rootCategorias = fxmlLoaderCategorias.load();
+        controladorCategorias = fxmlLoaderCategorias.getController();
+        sceneCategorias = new Scene(rootCategorias, 646, 394);
 
         FXMLLoader fxmlLoaderLogin = new FXMLLoader(TaskFlowApplication.class.getResource("login-view.fxml"));
-           // Parent rootLogin = fxmlLoaderLogin.load(); //Do not use this if this is the first scene lauched
-            controladorRegister = fxmlLoaderLogin.getController();
-            sceneLogin = new Scene(fxmlLoaderLogin.load(),646,394);
+        // Parent rootLogin = fxmlLoaderLogin.load(); //Do not use this if this is the first scene lauched
+        controladorRegister = fxmlLoaderLogin.getController();
+        sceneLogin = new Scene(fxmlLoaderLogin.load(), 646, 394);
 
         //Adding icon to application
         stage.getIcons().add(new Image(Objects.requireNonNull(TaskFlowApplication.class.getResourceAsStream("/logo.png"))));
 
-       // stage.initStyle(StageStyle.UNIFIED); not supported because I don't have MACOS :)
+        // stage.initStyle(StageStyle.UNIFIED); not supported because I don't have MACOS :)
 
         //Setting the tittle
         stage.setTitle("Task Flow!");
-         //No resize
+        //No resize
         stage.setResizable(false);
         //Launching scene
-            // stage.setScene(sceneHome);
+        // stage.setScene(sceneHome);
         stage.setScene(sceneLogin);
 
         stage.show();
